5edaf63a045d10a4916b1bc2467abdad29041dc4
Justtoobusy <alemu@mail.ru>
2019-04-21 13:25:38 +0300

    Fix

--- data.php
+++ data.php
@@ -1,1 +1,36 @@
 <?php
+$projects = [
+    "Входящие","Учеба","Работа","Домашние дела","Авто"
+];
+$tasks = [
+    [
+        'task' => 'Собеседование в IT компании',
+        'completion_date' => '01.12.2019',
+        'category' => 'Работа',
+        'is_done' => False
+    ],
+    [
+        'task' => 'Выполнить тестовое задание',
+        'completion_date' => '25.12.2019',
+        'category' => 'Работа',
+        'is_done' => False
+    ],
+    [
+        'task' => 'Сделать задание первого раздела',
+        'completion_date' => '22.12.2019',
+        'category' => 'Учеба',
+        'is_done' => True
+    ],
+    [
+        'task' => 'Встреча с другом',
+        'completion_date' => '20.04.2019',
+        'category' => '	Входящие',
+        'is_done' => False
+    ],
+    [
+        'task' => 'Купить корм для кота',
+        'completion_date' => 'Нет',
+        'category' => 'Домашние дела',
+        'is_done' => False
+    ]
+];
