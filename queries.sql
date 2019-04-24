b1ae39ebb40948c2bfb7914de19614112aeccf9a
Justtoobusy <alemu@mail.ru>
2019-04-24 21:04:07 +0300

    last changes

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ INSERT INTO users
 SET email = 'pelena66@mail.ru', username = 'Mother', PASSWORD = 'qwerty' ;
 --закидываем проекты
 INSERT INTO categories
-(title) VALUES ('Учеба','Входящие','Работа','Домашние дела','Авто');
+(title) VALUES ('Учеба'),('Входящие'),('Работа'),('Домашние дела'),('Авто');
 --добавляем задачи
 INSERT INTO tasks
 SET is_done = 0, title = 'Собеседование в IT компании', deadline = '2019-12-01', user_id ='1', category_id = '4';
