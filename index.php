ebe13f1ac4a26ab2a018d1bb0ac785ad1c53cc2d
Justtoobusy <alemu@mail.ru>
2019-05-06 22:01:41 +0300

    module6-task1 done

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+require 'helpers.php';
+require 'functions.php';
+require 'data.php';
+
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 
@@ -7,45 +11,29 @@ if (isset($_GET['category_id'])) {
     $category_id = $_GET['category_id'];
 }
 
-require 'helpers.php';
-require 'functions.php';
-require 'data.php';
-
 $con = mysqli_connect('localhost', 'root', '', 'doingsdone');
 mysqli_set_charset($con, 'utf8');
 
-$projects = getDataAll($con, 'SELECT id,title FROM categories', []);
+$sql = 'SELECT t.*,c.title as category_name,date_format(t.deadline,"%d.%m.%Y") as deadline FROM tasks t LEFT JOIN categories c ON t.category_id = c.id ';
+$param = [];
 if ($category_id) {
-    $tasks = getDataAll($con, "SELECT t.*,c.title as category_name,date_format(t.deadline,\"%d.%m.%Y\") as deadline FROM tasks t LEFT JOIN categories c ON t.category_id = c.id WHERE c.id = {$category_id}", []);
-
-} else {
-    $tasks = getDataAll($con, 'SELECT t.*,c.title as category_name,date_format(t.deadline,"%d.%m.%Y") as deadline FROM tasks t LEFT JOIN categories c ON t.category_id = c.id ', []);
+    $sql = "SELECT t.*,c.title as category_name,date_format(t.deadline,\"%d.%m.%Y\") as deadline FROM tasks t LEFT JOIN categories c ON t.category_id = c.id WHERE c.id = ?";
+$param = [$category_id];
 }
-$tasks_counts = getDataAll($con, 'SELECT COUNT(id) AS task_count ,category_id FROM tasks GROUP BY category_id', []);
-$tasks_count_projects = [];
-foreach ($tasks_counts as $tasks_count) {
-    $tasks_count_projects[$tasks_count['category_id']] = $tasks_count['task_count'];
-}
-
-foreach ($projects as &$project) {
-    $params = $_GET;
-    $params['category_id'] = $project['id'];
-    $scriptname = pathinfo(__FILE__, PATHINFO_BASENAME);
-    $query = http_build_query($params);
-    $project['url'] = "/" . $scriptname . "?" . $query;
-    $project['task_count'] = $tasks_count_projects[$project['id']] ?? 0;
-    $project['is_active'] = $project['id'] == $category_id;
-};
-$index_content = include_template('index.php', [
-    'projects' => $projects,
-    'tasks' => $tasks
-]);
-if ($category_id && !$tasks) {
+$tasks = getDataAll($con, $sql, $param);
+if (empty($tasks)) {
     http_response_code(404);
 }
+$projects = getDataAll($con, 'SELECT (SELECT COUNT(*) FROM tasks WHERE category_id = c.id ) AS task_count ,c.id, c.title FROM categories c  GROUP BY c.id', []);
+
+$index_content = include_template('index.php', [
+    'projects' => $projects,
+    'tasks' => $tasks,
+    'category_id' => $category_id
+]);
+
 print include_template('layout.php', [
     'content' => $index_content,
     'title' => 'Дела в порядке'
 ]);
-
 
